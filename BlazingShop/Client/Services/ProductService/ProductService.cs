dde83d59b15dc01155d19f1094c56610ebba03d3
LENOVO <LENOVO@DESKTOP-VG9LH9N>
2023-03-01 10:53:38 +0700

    Add project files.

---
+++ BlazingShop/Client/Services/ProductService/ProductService.cs
@@ -0,0 +1,46 @@
+﻿using BlazingShop.Shared;
+
+namespace BlazingShop.Client.Services.ProductService
+{
+    public class ProductService : IProductService
+    {
+        public List<Product> Products { get; set; } = new List<Product>();
+
+        public void LoadProducts()
+        {
+            Products = new List<Product>
+            {
+                new Product
+                {
+                    Id = 1,
+                    CategoryId = 1,
+                    Title = "The Hitchhiker's Guide to the Galaxy",
+                    Description = "The Hitchhiker's Guide to the Galaxy (sometimes referred to as HG2G,",
+                    Image = "https://upload.wikimedia.org/wikipedia/en/b/bd/H2G2_UK_front_cover.jpg",
+                    Price = 9.99m,
+                    OriginalPrice = 19.99m,
+                },
+        new Product
+        {
+            Id = 2,
+            CategoryId = 2,
+            Title = "The Hitchhiker's Guide to the Galaxy",
+            Description = "The Hitchhiker's Guide to the Galaxy (sometimes referred to as HG2G, HHGTTG, H2G2, or tHGttG) is a comedy science fiction series created by Douglas Adams",
+            Image = "https://upload.wikimedia.org/wikipedia/en/b/bd/H2G2_UK_front_cover.jpg",
+            Price = 9.99m,
+            OriginalPrice = 19.9m,
+        },
+        new Product
+        {
+            Id = 3,
+            CategoryId = 3,
+            Title = "Half-Life 2",
+            Description = "Half-Life 2 is a 2004 first-person shooter game developed and published by Valve. Like the original Half-Life, it combinesshooting, puzzles, and adds features such as vehicles and physics-based gameplay",
+            Image = "https://upload.wikimedia.org/wikipedia/en/2/25/Half-Life_2_cover.jpg",
+            Price = 8.19m,
+            OriginalPrice = 29.99m,
+        }
+            };
+        }
+    }
+}
